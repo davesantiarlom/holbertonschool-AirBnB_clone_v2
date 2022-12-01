b4523e7befce3a3953fa68d8391463583453893f
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-01 08:10:02 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,28 +1,24 @@
 #!/usr/bin/python3
-'''
-    Implementation of the User class which inherits from BaseModel
-'''
+"""This is the user class"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import String, Column
+from sqlalchemy import Column, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-import os
 
 
 class User(BaseModel, Base):
-    '''
-        Definition of the User class
-    '''
-    __tablename__ = "users"
-    if os.getenv('HBNB_TYPE_STORAGE') == 'db':
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128), nullable=True)
-        last_name = Column(String(128), nullable=True)
-        places = relationship("Place", passive_deletes=True, backref="user")
-        reviews = relationship("Review", passive_deletes=True, backref="user")
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
+    """This is the class for user
+    Attributes:
+    email: email address
+    password: password for you login
+    first_name: first name
+    last_name: last name
+    """
+    __tablename__ = 'users'
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
+    places = relationship('Place', backref='users')
+    reviews = relationship('Review', backref='user')
 
