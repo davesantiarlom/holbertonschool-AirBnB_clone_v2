12a74f1dde101e2cda48a155991c4f3ea35e80b1
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-01 08:04:02 -0500

    Update user.py

--- models/user.py
+++ models/user.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python3
-"""This is the user class"""
-from models.base_model import BaseModel, Base
+from os import getenv
+from models import *
+from models.base_model import Base, BaseModel
 from sqlalchemy import Column, String
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This is the class for user
-    Attributes:
-    email: email address
-    password: password for you login
-    first_name: first name
-    last_name: last name
-    """
-    __tablename__ = 'users'
-    email = Column(String(128), nullable=False)
-    password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=True)
-    last_name = Column(String(128), nullable=True)
-    places = relationship('Place', backref='users')
-    reviews = relationship('Review', backref='user')
+    if getenv('HBNB_TYPE_STORAGE', 'fs') == 'db':
+        __tablename__ = 'users'
+        email = Column(String(128), nullable=False)
+        password = Column(String(128), nullable=False)
+        first_name = Column(String(128), nullable=False)
+        last_name = Column(String(128), nullable=False)
+        places = relationship("Place", backref="user",
+                              cascade="all, delete, delete-orphan")
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
