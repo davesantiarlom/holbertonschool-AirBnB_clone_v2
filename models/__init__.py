29a57d38fd296d05802da2e7e7313b68edc0d000
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-01 07:56:03 -0500

    Update __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,26 @@
 #!/usr/bin/python3
-"""This module instantiates an object of class FileStorage"""
+"""create a unique FileStorage instance for your application"""
+"""
+None of these are needed
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+from models.user import User
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
+"""
+from os import getenv
 
+# Determine which storage type we want to access: db or file
+# and import the according class
+if getenv('HBNB_TYPE_STORAGE') == 'db':
+    from models.engine.db_storage import DBStorage
+    storage = DBStorage()
+else:
+    from models.engine.file_storage import FileStorage
+    storage = FileStorage()
 
-storage = FileStorage()
 storage.reload()
+
