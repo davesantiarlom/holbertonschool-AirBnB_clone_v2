03858b8740821dc1c4a2d59572cfec5eecfc90b8
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-01 07:55:28 -0500

    Update state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,33 @@
 #!/usr/bin/python3
-""" State Module for HBNB project """
-from models.base_model import BaseModel
+"""This is the state class"""
+import models
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
-class State(BaseModel):
-    """ State class """
-    name = ""
+class State(BaseModel, Base):
+    """This is the class for State
+    Attributes:
+        name: input name
+    """
+    __tablename__ = 'states'
+    name = Column(String(128), nullable=False)
+    # name = "" is no longer needed
+
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
+        cities = relationship('City', backref='state',
+                              cascade='all, delete-orphan')
+    else:
+        @property
+        def cities(self):
+            """ Return all City objects from FileStorage """
+            list_of_cities = []
+            for city in models.storage.all(City).values():
+                if self.id == city.state_id:
+                    list_of_cities.append(city)
+
+            return list_of_cities
+
