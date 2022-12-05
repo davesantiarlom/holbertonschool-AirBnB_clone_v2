f750cd37f4a7a1065e7a9496c1e75609f5734d1c
David Santiago Arias Lombana <57549591+davesantiarlom@users.noreply.github.com>
2022-12-05 09:48:28 -0500

    Create 2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" This script starts a Flask web application """
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ Print the following message upon connecting to Flask
+        at the above app.route """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """ Print the following message upon connecting to Flask
+        at the above app.route """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def fun_c(text):
+    """ Print the following message upon connecting to Flask
+        at the above app.route. Text is replaced with whatever
+        is passed in """
+    return "C {}".format(text).replace('_', ' ')
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
